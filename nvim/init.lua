01e192605bdf60362f07e640a53385e71e0a10be
Luca Zeuch <l-zeuch@email.de>
2022-06-25 11:19:09 +0200

    all: backup once more after ages
    Signed-off-by: Luca Zeuch <l-zeuch@email.de>

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,40 +1,40 @@
 local config = {
 
-  -- Set colorscheme
-  colorscheme = "nord",
+    -- Set colorscheme
+    colorscheme = "nord",
 
-  -- Default theme configuration
-  default_theme = {
-    -- Modify the color table
-    colors = {
-      fg = "#abb2bf",
+    -- Default theme configuration
+    default_theme = {
+        -- Modify the color table
+        colors = {
+            fg = "#abb2bf",
+        },
+        -- Modify the highlight groups
+        highlights = function(highlights)
+            local C = require "default_theme.colors"
+
+            highlights.Normal = { fg = C.fg, bg = C.bg }
+            return highlights
+        end,
     },
-    -- Modify the highlight groups
-    highlights = function(highlights)
-      local C = require "default_theme.colors"
 
-      highlights.Normal = { fg = C.fg, bg = C.bg }
-      return highlights
-    end,
-  },
-
-  -- Disable AstroVim ui features
-  ui = {
-    nui_input = true,
-    telescope_select = true,
-  },
-
-  -- Modify which-key registration
-  ["which-key"] = {
-    -- Add bindings to the normal mode <leader> mappings
-    register_n_leader = {
-      -- ["N"] = { "<cmd>tabnew<cr>", "New Buffer" },
-      ["fl"] =  { "<cmd>set list!<CR>", "Toggle Whitespaces"},
+    -- Disable AstroVim ui features
+    ui = {
+        nui_input = true,
+        telescope_select = true,
     },
-  },
 
-  -- Extend LSP configuration
-  lsp = {
+    -- Modify which-key registration
+    ["which-key"] = {
+        -- Add bindings to the normal mode <leader> mappings
+        register_n_leader = {
+            -- ["N"] = { "<cmd>tabnew<cr>", "New Buffer" },
+            ["fl"] =  { "<cmd>set list!<CR>", "Toggle Whitespaces"},
+        },
+    },
+
+    -- Extend LSP configuration
+    lsp = {
     -- add to the server on_attach function
     -- on_attach = function(client, bufnr)
     -- end,
@@ -44,108 +44,110 @@ local config = {
     --   server:setup(opts)
     -- end
 
-    -- Add overrides for LSP server settings, the keys are the name of the server
-    ["server-settings"] = {
-      yamlls = {
-        settings = {
-          yaml = {
-            schemas = {
-              ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
-              ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+        -- Add overrides for LSP server settings, the keys are the name of the server
+        ["server-settings"] = {
+            yamlls = {
+                settings = {
+                    yaml = {
+                        schemas = {
+                            ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
+                            ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+                        },
+                    },
+                },
             },
-          },
         },
-      },
     },
-  },
 
-  -- null-ls configuration
-  ["null-ls"] = function()
-    -- Formatting and linting
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim
-    local status_ok, null_ls = pcall(require, "null-ls")
-    if not status_ok then
-      return
-    end
-
-    -- Check supported formatters
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    local formatting = null_ls.builtins.formatting
-
-    -- Check supported linters
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-    local diagnostics = null_ls.builtins.diagnostics
-
-    null_ls.setup {
-      debug = false,
-      sources = {
-        -- Set a formatter
-        formatting.rufo,
-        -- Set a linter
-        diagnostics.rubocop,
-      },
-      -- NOTE: You can remove this on attach function to disable format on save
-      on_attach = function(client)
-        if client.resolved_capabilities.document_formatting then
-          vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+    -- null-ls configuration
+    ["null-ls"] = function()
+        -- Formatting and linting
+        -- https://github.com/jose-elias-alvarez/null-ls.nvim
+        local status_ok, null_ls = pcall(require, "null-ls")
+        if not status_ok then
+            return
         end
-      end,
-    }
-  end,
 
-  -- This function is run last
-  -- good place to configure mappings and vim options
-  polish = function()
-    local opts = { noremap = true, silent = true }
-    local map = vim.api.nvim_set_keymap
-    local set = vim.opt
-    local g = vim.g
+        -- Check supported formatters
+        -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+        local formatting = null_ls.builtins.formatting
 
-    -- Set options
-    set.relativenumber = false
-    set.compatible = false
-    set.textwidth = 80
-    set.shiftwidth = 4
-    set.expandtab = false
-    set.tabstop = 4
+        -- Check supported linters
+        -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+        local diagnostics = null_ls.builtins.diagnostics
 
-    set.laststatus = 2
+        null_ls.setup {
+            debug = false,
+            sources = {
+                -- Set a formatter
+                formatting.rufo,
+                -- Set a linter
+                diagnostics.rubocop,
+            },
+            -- NOTE: You can remove this on attach function to disable format on save
+            on_attach = function(client)
+                if client.resolved_capabilities.document_formatting then
+                    vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+                end
+            end,
+        }
+    end,
 
-    g.yagpdbcc_override_ft = 1
+    options = {
+        opt = {
+            relativenumber = false,
+            compatible = false,
+            textwidth = 80,
+            tabstop = 8,
+            softtabstop = 4,
+            shiftwidth = 4,
+            expandtab = true,
+            laststatus = 2,
+        },
+    },
 
-    g.ale_sign_error = ' '
-    g.ale_sign_warning = ' '
+    -- This function is run last
+    -- good place to configure mappings and vim options
+    polish = function()
+        local opts = { noremap = true, silent = true }
+        local map = vim.api.nvim_set_keymap
+        local g = vim.g
 
-    g.livepreview_previewer = 'xreader'
+        g.yagpdbcc_override_ft = 1
 
-    -- Set key bindings
-    map("n", "<C-s>", ":w!<CR>", opts)
+        g.ale_sign_error = ' '
+        g.ale_sign_warning = ' '
 
-    -- Set autocommands
-    vim.api.nvim_del_augroup_by_name "alpha_settings"
+        g.livepreview_previewer = 'xreader'
 
-    vim.api.nvim_create_augroup("remove_trailing_spaces", {})
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      desc = "Remove trailing whitespaces before writing",
-      group = "remove_trailing_spaces",
-      pattern = "*",
-      command = ":%s/\\s\\+$//e",
-    })
+        -- Set key bindings
+        map("n", "<C-s>", ":w!<CR>", opts)
 
-    vim.api.nvim_create_augroup("packer_conf", {})
-    vim.api.nvim_create_autocmd("BufWritePost", {
-      desc = "Run packersync on update of plugin file",
-      group = "packer_conf",
-      pattern = "plugins.lua",
-      command = "source <afile> | PackerSync",
-    })
+        -- Set autocommands
+        vim.api.nvim_del_augroup_by_name "alpha_settings"
 
-    vim.cmd [[
-      set listchars=tab:→\ ,space:·,nbsp:␣,trail:•,eol:¶,precedes:«,extends:»
+        vim.api.nvim_create_augroup("remove_trailing_spaces", {})
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            desc = "Remove trailing whitespaces before writing",
+            group = "remove_trailing_spaces",
+            pattern = "*",
+            command = ":%s/\\s\\+$//e",
+        })
 
-      filetype plugin indent on
-      syntax enable
-    ]]
-  end,
+        vim.api.nvim_create_augroup("packer_conf", {})
+        vim.api.nvim_create_autocmd("BufWritePost", {
+            desc = "Run packersync on update of plugin file",
+            group = "packer_conf",
+            pattern = "plugins.lua",
+            command = "source <afile> | PackerSync",
+        })
+
+        vim.cmd [[
+        set listchars=tab:→\ ,space:·,nbsp:␣,trail:•,eol:¶,precedes:«,extends:»
+
+        filetype plugin indent on
+        syntax enable
+        ]]
+    end,
 }
 return config
--- nvim/plugins/init.lua
+++ nvim/plugins/init.lua
@@ -1,42 +1,41 @@
 return {
-  { "andweeb/presence.nvim" },
+    { "andweeb/presence.nvim" },
 
-  { 'hrsh7th/cmp-vsnip' },
-  { 'hrsh7th/vim-vsnip' },
-  { 'onsails/lspkind-nvim' },
-  { 'dense-analysis/ale' },
-  { 'arcticicestudio/nord-vim' },
-  {
-    'hrsh7th/cmp-cmdline',
-    after = "nvim-cmp",
-    config = function()
-      local cmp = require "cmp"
-      local config = cmp.get_config()
-      table.insert(config.sources, { name = 'cmdline' })
-      cmp.setup(config)
-    end,
-  },
-  {
-    'l-zeuch/yagpdb.vim',
-    after = "nvim-cmp",
-    config = function()
-      local cmp = require "cmp"
-      local config = cmp.get_config()
-      table.insert(config.sources, { name = 'yagpdb-cc' })
-      cmp.setup(config)
-    end,
-  },
+    { 'hrsh7th/cmp-vsnip' },
+    { 'hrsh7th/vim-vsnip' },
+    { 'onsails/lspkind-nvim' },
+    { 'dense-analysis/ale' },
+    { 'arcticicestudio/nord-vim' },
+    { 'fatih/vim-go', run = ':goupdatebinaries', ft = 'go' },
+    { 'junegunn/vader.vim', cmd = 'vader', ft = 'vader' },
+    { 'xuhdev/vim-latex-live-preview', cmd = 'llpstartpreview' },
+    { 'editorconfig/editorconfig-vim' },
+    {
+        'hrsh7th/cmp-cmdline',
+        after = "nvim-cmp",
+        config = function()
+            local cmp = require "cmp"
+            local config = cmp.get_config()
+            table.insert(config.sources, { name = 'cmdline' })
+            cmp.setup(config)
+        end,
+    },
+    {
+        'l-zeuch/yagpdb.vim',
+        config = function()
+            local cmp = require "cmp"
+            local config = cmp.get_config()
+            table.insert(config.sources, { name = 'yagpdb-cc' })
+            cmp.setup(config)
+        end,
+    },
+    {
+        'sQVe/sort.nvim',
+        config = function()
+            require'sort'.setup({})
+        end,
+    },
 
-  { 'fatih/vim-go', run = ':GoUpdateBinaries', ft = 'go' },
-  { 'junegunn/vader.vim', cmd = 'Vader', ft = 'vader' },
-  { 'xuhdev/vim-latex-live-preview', cmd = 'LLPStartPreview' },
-  {
-  'sQVe/sort.nvim',
-  config = function()
-    require'sort'.setup({})
-  end,
-  },
-
-  ["declancm/cinnamon.nvim"] = { disable = true },
-  ["antoniemedac/FixCursorHold.nvim"] = { disable = true },
+    ["declancm/cinnamon.nvim"] = { disable = true },
+    ["Darazaki/indent-o-matic"] = { disable = true },
 }
